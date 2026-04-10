8e79edb3e3ceda7926ff25f1d65b56be52c807cc
mahadi <mojtaba.ahadi@aurobay.com>
2026-04-10 14:18:39 +0200

    update workflow.yml

--- check_new_release.py
+++ check_new_release.py
@@ -1,7 +1,17 @@
 import json
-import os
 from urllib.parse import urlencode
-from urllib.request import urlopen, Request
+from urllib.request import urlopen
+
+
+def read_tags_file():
+    with open("n8n_release_tags.txt", "r", encoding="UTF8") as f:
+        images = f.readlines()
+
+    tags = []
+    for image in images:
+        tag = image.split(":")[-1]
+        tags.append(tag)
+    return tags
 
 
 def get_n8n_release_version():
@@ -30,9 +40,7 @@ def get_docker_latest_version(namespace="mojtabaahadi", image="n8n", page_size=1
 
 
 if __name__ == "__main__":
-    docker_tags = get_docker_latest_version()
+    docker_tags = read_tags_file()
     n8n_latest_release = get_n8n_release_version()
     if n8n_latest_release not in docker_tags:
         print(n8n_latest_release)
-
-
